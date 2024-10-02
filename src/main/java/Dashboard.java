99f3fba2c394436674846e91108715ab1d7e7f09
thecolossalsparkle <thecolossalsparkle@gmail.com>
2024-10-02 08:03:29 +0530

    Basic dashboard layout

---
+++ src/main/java/Dashboard.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Dashboard {
+    public Dashboard() {
+        JFrame frame = new JFrame();
+        frame.setSize(1000, 700);
+        frame.setTitle("Dashboard");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
---
+++ src/main/java/DashboardTester.java
@@ -0,0 +1,5 @@
+public class DashboardTester {
+    public static void main(String[] args) {
+        Dashboard dashboard = new Dashboard();
+    }
+}
